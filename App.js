360d579b17882a47542b2491291bdec8ea87a55d
rachit <rachit.sharma@wundermanthompson.com>
2024-07-09 23:04:44 +0100

    Added dynamic bundling/chunking/lazy loading/code splitting/dynamic import - basically added multiple bundles in the project

--- App.js
+++ App.js
@@ -62,6 +62,7 @@ import MainLayout from './MainLayout';
 
 function App() {
     return (
+        
         <MainLayout />
     )
 }
--- Route.js
+++ Route.js
@@ -1,10 +1,26 @@
 import About from './src/components/About';
-import Main from './src/components/Main';
+//import Main from './src/components/Main';
 import MainLayout from './MainLayout';
 import ProductListing from './src/components/ProductListing';
 import { createBrowserRouter} from 'react-router-dom';
-import ProductDetails from './src/components/ProductDetails';
+//import ProductDetails from './src/components/ProductDetails';
+import { lazy, Suspense } from 'react';
+import Shimmer from './src/components/Shimmer';
 
+
+//Chunking
+//dynamic import
+//lazy loading
+// dynamic bundling
+// code splitting 
+// all above names are different but they are trying to resolve the same problem statement which is reduce the size of bundling or create multiple bundles
+
+const ProductDetails = lazy(function(){
+     return import("./src/components/ProductDetails");
+})
+const Main = lazy(function(){
+     return import("./src/components/Main");
+})
 export const appRouter = createBrowserRouter([
     {
          path:'/',
@@ -20,7 +36,9 @@ export const appRouter = createBrowserRouter([
             element: <ProductListing />
        },{
           path: '/product/:prodId',
-          element: <ProductDetails />
+          element: <Suspense fallback={<Shimmer/>}>
+                    <ProductDetails />
+               </Suspense>
      }]
     },
     
